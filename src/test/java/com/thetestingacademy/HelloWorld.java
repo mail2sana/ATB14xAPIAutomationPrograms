36cc286b13ef976ecba5f0d7294cd7be39d09b5a
SANGEETHA <mail2sana.v@gmail.com>
2025-12-05 20:14:12 +0530

    29 November 2025 |Maven | Hello

--- src/test/java/com/thetestingacademy/HelloWorld.java
+++ src/test/java/com/thetestingacademy/HelloWorld.java
@@ -1,4 +1,12 @@
 package com.thetestingacademy;
 
+import io.restassured.RestAssured;
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        RestAssured
+                .given().baseUri("https://www.google.com/").log().all()
+                .when().get()
+                .then().log().all();
+    }
 }
